ca895a1e44e4fbbac3ed8a4d62ddc4b53580890f
michaelrambeau <mikeair@gmail.com>
2017-08-20 11:35:22 +0900

    Fix keyword parsing

--- src/parse-user-input.js
+++ src/parse-user-input.js
@@ -1,10 +1,19 @@
 const debug = require('debug')('parse')
 
+// Parse user answer to a given question
+// using question "keywords" to search for several terms related to a given keyword
+// Example of question: (see `src/survey/*.json` files):
+// {
+//  "key": "other",
+//  "text": "Other Front-End Frameworks",
+//  "keywords": [{ "text": "preact", "shortlist": true }]
+// }
+// Return the array of found keywords
 const parseUserInputSurvey = question => input => {
   const words = question.keywords
   const found = words
     ? words.filter(word => {
-        const re = new RegExp(word.query || word, 'i')
+        const re = new RegExp(word.query || word.text || word, 'i')
         return re.test(input)
       })
     : []
