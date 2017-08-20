5f943125e9cdb13f9706ab09c0211f6ec51a6d00
michaelrambeau <mikeair@gmail.com>
2017-08-20 09:14:35 +0900

    Split answers and meta results into separate files

--- src/aggregation/aggregate-folder.js
+++ src/aggregation/aggregate-folder.js
@@ -2,6 +2,8 @@ const fs = require('fs-extra')
 const path = require('path')
 const aggregateFiles = require('./aggregate-files')
 
+// Read data from ALL files in a given folder, aggregate data and return a single Object
+// The aggregation is specified with the `responseReducer` (the reducer applied to all responses)
 async function aggregateFolder({
   survey,
   folderPath,
@@ -11,7 +13,10 @@ async function aggregateFolder({
 }) {
   const filenames = await fs.readdir(folderPath)
   if (logger)
-    logger.info(`${filenames.length} CSV files to process'`, filenames)
+    logger.info(
+      `${filenames.length} CSV files to process inside ${folderPath} folder`,
+      filenames
+    )
   const filepaths = filenames.map(filename => path.join(folderPath, filename))
   return aggregateFiles({
     filepaths,
